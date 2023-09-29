d41eb586aea3d1c184dcdfcaf47357c38dcc6f48
FSEPBEAR <145767380+FSEPBEAR@users.noreply.github.com>
2023-09-30 00:26:12 +0900

    Add files via upload

---
+++ embedded/P2/psi22/PBEAR-base/synth/duet_floorIsOrdered_0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=~/.opam/4.08.0/lib/z3:$LD_LIBRARY_PATH
+timeout 600 duet -max_size 512 -max_height 15 -init_comp_size 1 results/elevator/p1/no_alarm/synth/floorIsOrdered_0.sl > results/elevator/p1/no_alarm/synth/result_floorIsOrdered_0 2>&1
+
